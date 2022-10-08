ec9aa022dd81af30b38bdd530c723828e1b62ed2
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2022-10-08 14:03:25 +1000

    added drop from doc to board

--- src/app/editor/dock/dock.component.ts
+++ src/app/editor/dock/dock.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from "@angular/core";
 interface DockElement{
     content: string,
     type: string,
-    tooltip:string
+    tooltip:string,
+    draggable:boolean;
 }
 
 @Component({
@@ -15,7 +16,7 @@ interface DockElement{
 export class DockComponent {
 
 
-    //class="drag-drawflow" draggable="true" (dragstart)="drag($event)" data-node="and-gate"
+    //class="drag-drawflow" draggable="true" \data-node="and-gate"
 
     @Input() dragCallBack:Function;
 
@@ -23,57 +24,74 @@ export class DockComponent {
         {
             content: 'fas fa-cubes',
             type: 'components',
-            tooltip: 'All components'
+            tooltip: 'All components',
+            draggable: false
         },
         {
             content: 'i',
-            type: 'input',
-            tooltip: 'Variable input'
+            type: 'input-component-on',
+            tooltip: 'Variable input',
+            draggable: true
         },
         {
             content: 'o',
-            type: 'output',
-            tooltip: 'Output'
+            type: 'output-component',
+            tooltip: 'Output',
+            draggable: true
+        },
+        {
+            content: '!',
+            type: 'not-gate',
+            tooltip: 'Not gate',
+            draggable: true
         },
         {
             content: '&',
             type: 'and-gate',
-            tooltip: 'And gate'
+            tooltip: 'And gate',
+            draggable: true
         },
         {
             content: '|',
-            type: 'or',
-            tooltip: 'Or gate'
+            type: 'or-gate',
+            tooltip: 'Or gate',
+            draggable: true
         },
         {
             content: '^',
-            type: 'xor',
-            tooltip: 'Xor gate'
+            type: 'xor-gate',
+            tooltip: 'Xor gate',
+            draggable: true
         },
         {
             content: 'fas fa-microchip',
             type: 'custom',
-            tooltip: 'Custom compontent'
+            tooltip: 'Custom compontent',
+            draggable: true
         },
         {
             content: 'fas fa-search-minus',
             type: 'zoom-out',
-            tooltip: 'Zoom out'
+            tooltip: 'Zoom out',
+            draggable: false
         },
         {
             content: 'fas fa-search',
             type: 'zoom-clear',
-            tooltip: 'Reset zoom'
+            tooltip: 'Reset zoom',
+            draggable: false
         },
         {
             content: 'fas fa-search-plus',
             type: 'zoom-in',
-            tooltip: 'Zoom in'
+            tooltip: 'Zoom in',
+            draggable: false
         },
         {
             content: 'fas fa-save',
             type: 'save',
-            tooltip: 'Save board'
+            tooltip: 'Save board',
+            draggable: false
         }
     ];
   }
